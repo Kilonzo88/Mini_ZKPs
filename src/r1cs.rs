4db6082bca0fcfc825477fc1d8d1e2e54b88b9f1
Dennis <kilonzod19@gmail.com>
2026-01-05 14:13:27 +0300

    Laid down the foundation by defining the data structures

---
+++ src/r1cs.rs
@@ -0,0 +1,60 @@
+use num_bigint::BigInt;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
+
+#[derive(Serialize, Deserialize)]
+pub struct Variable {
+    pub index: usize,
+    pub value: BigInt,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Operation {
+    Add,
+    Mul,
+    Hash,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Constraint {
+    pub left: Vec<(Variable, BigInt)>,
+    pub right: Vec<(Variable, BigInt)>,
+    pub output: Vec<(Variable, BigInt)>,
+    pub operation: Operation,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct R1CS {
+    pub variables: Vec<Variable>,
+    pub constraints: Vec<Constraint>,
+}
+
+impl R1CS {
+    pub fn new() -> Self {
+        Self {
+            variables: Vec::new(),
+            constraints: Vec::new(),
+        }
+    }
+
+    pub fn add_constraint(
+        &mut self,
+        left: Vec<(Variable, BigInt)>,
+        right: Vec<(Variable, BigInt)>,
+        output: Vec<(Variable, BigInt)>,
+        operation: Operation,
+    ) {
+        let constraint = Constraint {
+            left,
+            right,
+            output,
+            operation,
+        };
+        self.constraints.push(constraint);
+    }
+
+    pub fn add_variable(&mut self, variable: Variable) {
+        self.variables.push(variable);
+    }
+}
