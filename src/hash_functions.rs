9645881f2fe1025ed81d12fdbda960e09f4438a7
Dennis <kilonzod19@gmail.com>
2026-01-13 12:12:46 +0300

    Initial commit

--- src/hash_functions.rs
+++ src/hash_functions.rs
@@ -1,15 +1,15 @@
-use num_bigint::BigInt;
+use crate::field::FieldElement;
 
 ///Define a trait for hash functions
 pub trait HashFunction {
-    fn hash(&self, a: &BigInt, b: &BigInt) -> BigInt;
+    fn hash(&self, a: &FieldElement, b: &FieldElement) -> FieldElement;
 }
 
 /// A simple addition hash function(for demo purposes only)
 pub struct SimpleAddHash;
 
 impl HashFunction for SimpleAddHash {
-    fn hash(&self, a: &BigInt, b: &BigInt) -> BigInt {
+    fn hash(&self, a: &FieldElement, b: &FieldElement) -> FieldElement {
         a + b
     }
 }
@@ -18,7 +18,7 @@ impl HashFunction for SimpleAddHash {
 pub struct CustomHash;
 
 impl HashFunction for CustomHash {
-    fn hash(&self, a: &BigInt, b: &BigInt) -> BigInt {
-        a + b
+    fn hash(&self, a: &FieldElement, b: &FieldElement) -> FieldElement {
+        a + b // Placeholder, could be more complex
     }
 }
